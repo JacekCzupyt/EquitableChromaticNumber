8fc5f7347b73aad087141d43b2a5f06180d53dde
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-01 14:10:48 +0200

    Unit tests first draft

---
+++ EquitableChromaticNumber.UnitTests/EquitableChromaticNumber.UnitTests.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../EquitableChromaticNumber/Graph.h"
+#include "../EquitableChromaticNumber/Graph.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EquitableChromaticNumberUnitTests
+{
+	TEST_CLASS(EquitableChromaticNumberUnitTests)
+	{
+	public:
+
+		TEST_METHOD(ConstructorAndSize)
+		{
+			using namespace ecnGraph;
+			int n = 5;
+			Graph g(n);
+			Assert::AreEqual(g.Size(), n);
+		}
+	};
+}
