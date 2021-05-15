2e9777c162400bdd464e1f6a883bb5ba5e22c93d
JacekCzupyt <kuba.czupyt@gmail.com>
2021-05-15 16:33:41 +0200

    Added IsColoredSmallGraphs test for Bits

---
+++ EquitableChromaticNumber.UnitTests/BitsColoringAlgorithm.UnitTests.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../EquitableChromaticNumber/BitsColoringAlgorithm.h"
+#include "../EquitableChromaticNumber/ColoredGraph.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EquitableChromaticNumberUnitTests {
+	using namespace ecnGraph;
+
+	TEST_CLASS(BitsColoringAlgorithmUnitTests) {
+	public:
+		TEST_METHOD(IsColoredSmallGraphs) {
+			BitsColoringAlgorithm Bits(0.01f);
+			srand(0);
+			for (int i = 0; i < 100; i++) {
+				ColoredGraph g = GenerateRandomGraph(5 + i % 10, (double)rand()/RAND_MAX);
+				Bits.Color(g);
+				Assert::IsTrue(g.IsColored());
+			}
+		}
+
+	private:
+		ColoredGraph GenerateRandomGraph(int n, double density) {
+			ColoredGraph graph(n);
+			for (int i = 0; i < n; i++) {
+				for (int j = i + 1; j < n; j++) {
+					if ((double)rand() / RAND_MAX < density) {
+						graph.AddEdge(i, j);
+					}
+				}
+			}
+			return graph;
+		}
+	};
+}
