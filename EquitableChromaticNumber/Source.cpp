83875ddc49d70f7377220c187804dd4afcfe0023
JacekCzupyt <kuba.czupyt@gmail.com>
2021-05-27 00:04:24 +0200

    Refactor source

--- EquitableChromaticNumber/Source.cpp
+++ EquitableChromaticNumber/Source.cpp
@@ -14,8 +14,7 @@
 
 using namespace ecnGraph;
 
-int main() {
-
+ColoredGraph getGz() {
 	ColoredGraph gz(12);
 	gz.AddEdge(0, 1);
 	gz.AddEdge(0, 2);
@@ -28,40 +27,21 @@ int main() {
 	gz.AddEdge(11, 6);
 	gz.AddEdge(11, 7);
 	gz.AddEdge(11, 8);
+	return gz;
+}
 
-	ColoredGraph gk(12);
-	gk.AddEdge(0, 1);
-	gk.AddEdge(0, 2);
-	gk.AddEdge(0, 10);
-	gk.AddEdge(0, 11);
-	gk.AddEdge(0, 9);
-	gk.AddEdge(3, 10);
-	gk.AddEdge(4, 10);
-	gk.AddEdge(5, 10);
-	gk.AddEdge(11, 6);
-	gk.AddEdge(11, 7);
-	gk.AddEdge(11, 8);
 
-	ColoredGraph gk2(12);
-	gk2.AddEdge(0, 1);
-	gk2.AddEdge(0, 2);
-	gk2.AddEdge(0, 10);
-	gk2.AddEdge(0, 11);
-	gk2.AddEdge(0, 9);
-	gk2.AddEdge(3, 10);
-	gk2.AddEdge(4, 10);
-	gk2.AddEdge(5, 10);
-	gk2.AddEdge(11, 6);
-	gk2.AddEdge(11, 7);
-	gk2.AddEdge(11, 8);
+int main() {
 
 	JfkAlgorithm jfk;
 	GreedyAlgorithm grd;
 	BitsColoringAlgorithm bits1(0.2f);
 
-	int chromaticNumber1 = jfk.Color(gz);
-	int chromaticNumber2 = grd.Color(gk);
-	int chromaticNumber3 = bits1.Color(gk2);
+	ColoredGraph g1 = getGz(), g2 = getGz(), g3 = getGz();
+
+	int chromaticNumber1 = jfk.Color(g1);
+	int chromaticNumber2 = grd.Color(g2);
+	int chromaticNumber3 = bits1.Color(g3);
 
 	std::cout << "jfk: Chromatic number equals " << chromaticNumber1 << ".\n";;
 	std::cout << "greedy: Chromatic number equals " << chromaticNumber2 << ".\n";;
